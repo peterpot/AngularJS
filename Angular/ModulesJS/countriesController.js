545ba1548d7d624837e107121371b2bf1c55e225
peterpot <peter.potocnik@windowslive.com>
2017-05-23 13:34:12 +0100

    Add files via upload

---
+++ Angular/ModulesJS/countriesController.js
@@ -0,0 +1,26 @@
+﻿mainApp.controller("countriesController", function ($scope, $http) {
+
+    //$https.get("/api/countries").success(function (response) {
+    //    alert("returned countries!");
+    //    $scope.countries = response;
+    //});
+
+    $scope.showCountry = function (country) {
+        alert(country);
+    }
+
+    $http({
+        method: 'GET',
+        url: '/api/countries'
+    }).then(function successCallback(response) {
+        $scope.countries = response.data;
+
+        //[
+        //   { Id: '204', Name: "United Kingdom" },
+        //   { Id: '205', Name: "United States" }
+        //];
+
+    }, function errorCallback(response) {
+        alert("error response!");
+    });
+});
